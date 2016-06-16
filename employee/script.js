ff3de9d1257b9428d8ba02ee2779ddf26c3c147d
mpilcher90 <mpilcher90@gmail.com>
2016-06-16 20:28:22 +0000

    add javascript and php files

--- employee/script.js
+++ employee/script.js
@@ -1,6 +1,33 @@
 $(document).ready(function(){
-  $(':submit').on('click', function() {
-    
-    return false; 
+  $(':submit').on('click', function() { 
+    var button = $(this).val();
+    $.ajax({ 
+      url: 'test.php', 
+      data: 'button=' + $(this).val(), 
+      dataType: 'json', 
+    })
+    .done(function(data) { // Data variable contains the data from serverside
+      $('#employees').html(''); 
+				
+      if (button == 'all') { 
+        for (var i in data.programmers) {
+          $('#employees').append('Programmer: ' + data.programmers[i] + '<br>');
+        }
+        for (var i in data.engineers) {
+          $('#employees').append('Engineer: ' + data.engineers[i] + '<br>');
+        }
+      }
+      else if (button == 'programmers') { 
+        for (var i in data) {
+          $('#employees').append('Programmer: ' + data[i] + '<br>');
+        }
+      }
+      else if (button == 'engineers') { 
+        for (var i in data) {
+          $('#employees').append('Engineer: ' + data[i] + '<br>');
+        }
+      }
+    });
+    return false; // Keeps the page from refreshing 
   });
 });
--- employee/test.php
+++ employee/test.php
@@ -1,5 +1,26 @@
 <?php
-$button = $_GET['button'];
+ 
 
+$button = $_GET['button'];
+ 
+
+$programmers = array('Jack Hopkins', 'Rent Cantro', 'Angel Karp');
+$engineers = array('John Cave', 'Cava Nate', 'Chappy Boar');
+ 
+
+$employeetable = array(
+  "programmers" => $programmers,
+  "engineers" => $engineers,
+);
+
+if ($button == "programmers") {
+  print json_encode($programmers);
+}
+else if ($button == "engineers") {
+  print json_encode($engineers);
+}
+else {
+  print json_encode($employeetable);
+}
  
 ?>
